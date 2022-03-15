dd18891c19d3cff8fb66d2229074ca5dfecf4431
TheFirstHokage <kendrickutwhite@gmail.com>
2022-03-15 13:37:36 -0700

    initial commit

---
+++ pet-clinic-web/src/main/java/master/spring/springpetclinicrestart/SpringPetClinicRestartApplication.java
@@ -0,0 +1,13 @@
+package master.spring.springpetclinicrestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicRestartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicRestartApplication.class, args);
+	}
+
+}
---
+++ pet-clinic-web/src/test/springpetclinicrestart/SpringPetClinicRestartApplicationTests.java
@@ -0,0 +1,13 @@
+package master.spring.springpetclinicrestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicRestartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
